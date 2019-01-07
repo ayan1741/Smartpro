ca008022023ae4401861f8c687ada445bafdb991
Mehmet Ayan <mehmet.ayan1741@gmail.com>
2019-01-07 10:01:45 +0300

    Email Dogrulama Projesi Eklendi

---
+++ C#/Koleksiyonlar/ArrayList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "dlkflsjl";
+            char [] dizi=a.ToCharArray();
+            var yeni=dizi.Reverse();
+            Console.WriteLine(yeni.GetType());
+        }
+    }
+}
