d09d0d6ff9ead1ea203367e3e99c4447fc89f4c5
Mehmet Ayan <mehmet.ayan1741@gmail.com>
2019-01-08 11:41:08 +0300

    Çeviri projesi eklendi

---
+++ C#/Koleksiyonlar/HashTable_Sorular/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable_Sorular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable sozluk = new Hashtable() {
+                {"Bugun","Today"},
+                {"Yarin","Tommorrow"},
+                {"Hava","Wheather" },
+                {"Gokyuzu","Sky" },
+                {"Karli","Snowy" },
+                {"Gunesli","Sunny" },
+                {"Bulutlu","Cloudy" },
+                {"Acik","Clear" },
+                {"Kapali","Cloudy" },
+            };
+            string cumle = "Bugun Hava Çok Bulutlu";
+            var kelimeler = cumle.Split(' ');
+            string ceviri = "";
+            
+            foreach (string kelime in kelimeler)
+            {
+                bool isTranslated = false;
+                foreach (DictionaryEntry item in sozluk)
+                {
+                    if (kelime==item.Key.ToString())
+                    {
+                        ceviri = ceviri + " " + item.Value;
+                        isTranslated = true;
+                    }
+                   
+                }
+                if (!isTranslated)
+                {
+                    ceviri = ceviri + " **** ";
+                }
+            }
+            Console.WriteLine(ceviri);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.Write("Cümle :  ");
+            //string cumle = Console.ReadLine();
+            //var words = cumle.Split();
+            //string translated = "";
+            //bool isTranslated = false;
+            //foreach (var word in words)
+            //{
+            //    foreach (DictionaryEntry item in dictionary)
+            //    {
+            //        if (word.ToLower()==(item.Key).ToString().ToLower())
+            //        {
+            //            translated = translated + " " + (item.Value).ToString();
+            //            isTranslated = true;
+            //        }
+            //    }
+            //    if (!isTranslated)
+            //    {
+            //        translated = translated + "  **** ";
+            //    }
+            //    isTranslated = false;       
+            //}
+            //Console.WriteLine("Çeviri :{0}",translated);
+        }
+    }
+}
---
+++ C#/Koleksiyonlar/HashTable_Tanımlama/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable_Tanımlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hashtable tanımlama
+            Hashtable htable = new Hashtable();
+            Console.WriteLine($"Count :{htable.Count}");
+
+            //Add metodu
+            htable.Add(1, "Bir");
+            htable.Add(2, "İki");
+            htable.Add(3, "Üç");
+            htable.Add(4, "Dört");
+            htable.Add("Beş", "5");
+
+            //Ekrana yazdırma
+            foreach (DictionaryEntry item in htable)
+            {
+                Console.WriteLine(item.Key + "     " + item.Value);
+                //Console.WriteLine(((DictionaryEntry)item).);
+            }
+
+            //silme işlemleri
+            htable.Remove("Beş");
+            //Beş Silindikten sonra
+            Console.WriteLine("Beş Silindikten sonra");
+            foreach (DictionaryEntry item in htable)
+            {
+                Console.WriteLine(item.Key + "     " + item.Value);
+                //Console.WriteLine(((DictionaryEntry)item).);
+            }
+
+            //Contains 
+
+            Console.WriteLine("Key: 7 değeri var mı?");
+            if (htable.ContainsKey(7))
+            {
+                Console.WriteLine("7 değeri Var");
+            }
+            else
+            {
+                Console.WriteLine("7 değeri Yok");
+            }
+            //-----value-----
+            Console.WriteLine("Value: iki değeri var mı?");
+            if (htable.ContainsValue("İki"))
+            {
+                Console.WriteLine("iki değeri Var");
+            }
+            else
+            {
+                Console.WriteLine("iki değeri Yok");
+            }
+            //Keys ve Values ları arraye atma
+            var array = new string[10];
+            ArrayList liste = new ArrayList();
+            htable.Values.CopyTo(array, 0);
+
+            //
+
+
+            foreach (var item in htable.Keys)
+            {
+                Console.WriteLine(htable.Keys);
+            }
+        }
+    }
+}
