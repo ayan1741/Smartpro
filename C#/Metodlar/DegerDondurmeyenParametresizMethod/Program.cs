7a46cd5031ec9f5fabbcdaa92009f2f75a00d57b
Mehmet Ayan <mehmet.ayan1741@gmail.com>
2019-01-09 10:06:36 +0300

    Dönüş degeri olmayan metodlar

---
+++ C#/Metodlar/DegerDondurmeyenParametresizMethod/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerDondurmeyenParametresizMethod
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //Geri dönüş değeri ve parametre-listesi boş olan, ekrana “Merhaba Dünya” yazdıran metodu tanımlayıp program içerisinde kullanımına bir örnek veriniz.
+            //#region Soru 1
+            //MerhabaDunyaYaz();
+
+            //#endregion
+            //----------------------------------------------------------------------------------------
+            //Klavyeden girilen bir tam sayının karesini ekrana yazan  metodu ve bu metodun program içerisinde kullanımını gösteren programın kodunu yazınız.
+            #region MyRegion
+            Console.Write("Bir Sayı Gir:");
+            int sayi=int.Parse(Console.ReadLine());
+            KareAl2(sayi);
+            #endregion
+
+            //Klavyeden girilen bir mesajı ekrana N defa yazdıran metodun kodunu yazınız.
+            #region MyRegion
+            Console.Write("Yazı:");
+            string yazi = Console.ReadLine();
+            Console.Write("Kaç Defa:");
+            int kacDefa = int.Parse(Console.ReadLine());
+            NDefaYaz(yazi, kacDefa);
+            #endregion
+        }
+
+        static void NDefaYaz(string neYazilacak,int kacDefaYazilacak)
+        {
+            for (int i = 0; i < kacDefaYazilacak; i++)
+            {
+                Console.WriteLine(neYazilacak);
+            }
+        }
+
+        static void MerhabaDunyaYaz()
+        {
+            Console.WriteLine("Merhaba Dünya");
+        }
+
+        static void KareAl(int sayi)
+        {
+            Console.WriteLine($"Verilen {sayi}'nın karesi {sayi * sayi}'dır.");
+        }
+
+        static void KareAl2(int sayi)
+        {
+            Console.WriteLine($"Verilen {sayi}'nın karesi {sayi * sayi}'dır.");
+        }
+
+    }
+}
