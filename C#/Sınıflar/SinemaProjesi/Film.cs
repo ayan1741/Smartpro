e3b8a93b3c5fa353bfee21b2c887cc8e96f56d97
Mehmet Ayan <mehmet.ayan1741@gmail.com>
2019-01-15 14:14:25 +0300

    Sınıf hızlı tanımlama eklendi

---
+++ C#/Sınıflar/SinemaProjesi/Film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaProjesi
+{
+    class Film
+    {
+        public string Ad { get; set; }
+        public int Yıl { get; set; }
+    }
+}
